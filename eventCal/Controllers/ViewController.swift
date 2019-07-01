09232264318a4c0a4f998453e04b917385ecfec5
Harsh_iOS <harsh.hbs@gmail.com>
2019-07-01 16:55:44 -0400

    calanderAdded

--- eventCal/Controllers/ViewController.swift
+++ eventCal/Controllers/ViewController.swift
@@ -11,32 +11,41 @@ import EventKit
 import JTAppleCalendar
 class ViewController: UIViewController {
     // 1
+    let boarderColor:CGColor = UIColor(red: 61/255, green: 197/255, blue: 202/255, alpha: 1).cgColor
+    
     let eventStore = EKEventStore()
     var calanderKeys:[String:String] = [:]
-   
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    let formatter = DateFormatter()
+    var eventFlag:Int = 0
+    var todayFlag:Int = 0
+    var todayDate:Date = Date()
+     var dateArr:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        todayDate = Date()
         // 2
-        switch EKEventStore.authorizationStatus(for: .event) {
-        case .authorized:
-           // insertEvent(store: eventStore)
-            fetchEventsFromCalendar(calendarTitle: "Harsh")
-        case .denied:
-            print("Access denied")
-        case .notDetermined:
-            // 3
-            eventStore.requestAccess(to: .event, completion:
-                {[weak self] (granted: Bool, error: Error?) -> Void in
-                    if granted {
-                        self!.insertEvent(store: self!.eventStore)
-                    } else {
-                        print("Access denied")
-                    }
-            })
-        default:
-            print("Case default")
-        }
+//        switch EKEventStore.authorizationStatus(for: .event) {
+//        case .authorized:
+//           // insertEvent(store: eventStore)
+//            fetchEventsFromCalendar(calendarTitle: "Harsh")
+//        case .denied:
+//            print("Access denied")
+//        case .notDetermined:
+//            // 3
+//            eventStore.requestAccess(to: .event, completion:
+//                {[weak self] (granted: Bool, error: Error?) -> Void in
+//                    if granted {
+//                        self!.insertEvent(store: self!.eventStore)
+//                    } else {
+//                        print("Access denied")
+//                    }
+//            })
+//        default:
+//            print("Case default")
+//        }
+      //  self.view.isOpaque = false
+        setupCalender()
        // assignLocalizestring(key: "Welcome", label: welcomeMessageLbl)
         // Do any additional setup after loading the view.
     }
@@ -163,3 +172,149 @@ extension ViewController{
         
     }
 }
+extension ViewController : JTAppleCalendarViewDelegate,JTAppleCalendarViewDataSource
+{
+    func setupCalender(){
+        calendarView.visibleDates{(visibleDates) in
+            self.setupViewsOfCalender(visibleDates: visibleDates)
+        }
+    }
+    func setupViewsOfCalender(visibleDates: DateSegmentInfo){
+        let date = visibleDates.monthDates.first!.date
+        self.formatter.dateFormat = "yyyy"
+        print(self.formatter.string(from: date))
+       // self.CalenderTitleOutlet.title = self.formatter.string(from: date)
+        self.formatter.dateFormat = "MMMM"
+      //  self.CalenderTitleOutlet.title  = self.formatter.string(from: date) + " " + self.CalenderTitleOutlet.title!
+        guard let startDate = visibleDates.monthDates.first?.date else {
+            return
+        }
+       
+    }
+    func handleCellTextColor(view: JTAppleCell?,cellState:CellState)
+    {
+        guard let validCell = view as? CalenderCell else {return}
+        if(cellState.isSelected)
+        {
+            validCell.dateLabel.textColor = UIColor.black
+        }
+        else{
+            if(cellState.dateBelongsTo == .thisMonth)
+            {
+                validCell.dateLabel.textColor = UIColor.black
+                if(todayFlag == 1)
+                {
+                    todayFlag = 0
+                    validCell.dateLabel.textColor = UIColor.yellow
+                    
+                    validCell.dateLabel.underline()
+                }
+                else if(eventFlag == 1)
+                {
+                    eventFlag = 0
+                    validCell.dateLabel.textColor = UIColor.green
+                    validCell.selectedView.isHidden = false
+                    validCell.selectedView.backgroundColor = UIColor.clear
+                    validCell.layer.borderWidth = 1
+                    validCell.layer.borderColor = boarderColor
+                    
+                }
+            }
+            else{
+                validCell.dateLabel.textColor = UIColor.blue
+            }
+            
+        }
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        setupViewsOfCalender(visibleDates: visibleDates)    }
+    
+    func handleCellSelected(view: JTAppleCell?,cellState:CellState)
+    {
+        guard let validCell = view as? CalenderCell else {return}
+        
+        if(cellState.isSelected)
+        {
+            
+            validCell.selectedView.isHidden = false
+            
+        }
+        else{
+            
+           // validCell.selectedView.isHidden = true
+        }
+        
+    }
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        guard cell is CalenderCell else {return}
+        
+    }
+    
+    //func setupViewOfCalender()
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        let startDate = Calendar.current.date(byAdding: .month, value: -3, to: Date())
+        let endDate = Calendar.current.date(byAdding: .month, value: 11, to: Date())
+        
+        let parameters = ConfigurationParameters(startDate: startDate!, endDate: endDate!)
+        calendar.scrollToDate(Date())
+        return parameters
+    }
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalenderCell", for: indexPath) as! CalenderCell
+        cell.dateLabel.text = cellState.text
+        var comapreDate:String = " "
+        var todayDateStr:String = " "
+        self.formatter.dateFormat = "yyyy/MM/dd"
+        comapreDate = formatter.string(from: date)
+        todayDateStr = formatter.string(from: todayDate)
+        
+        if(comapreDate == todayDateStr)
+        {
+            todayFlag = 1
+        }
+        else if(dateArr.contains(comapreDate))
+        {
+            eventFlag = 1
+        }
+        
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        return cell
+    }
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextColor(view: cell, cellState: cellState)
+        handleCellSelected(view: cell, cellState: cellState)
+    }
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextColor(view: cell, cellState: cellState)
+        handleCellSelected(view: cell, cellState: cellState)
+    }
+    
+    
+}
+
+extension UILabel{
+    
+    func makeOutLine(oulineColor: UIColor, foregroundColor: UIColor){
+        let strokeTextAttributes = [
+            NSAttributedString.Key.strokeColor : oulineColor,
+            NSAttributedString.Key.foregroundColor : foregroundColor,
+            NSAttributedString.Key.strokeWidth : -4.0,
+            NSAttributedString.Key.font : self.font
+            ] as [NSAttributedString.Key : Any]
+        self.attributedText = NSMutableAttributedString(string: self.text ?? "", attributes: strokeTextAttributes)
+    }
+    
+    func underline() {
+        if let textString = self.text {
+            let attributedString = NSMutableAttributedString(string: textString); attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
+            attributedText = attributedString
+        }
+    }
+}
+
---
+++ eventCal/View/CalenderCell.swift
@@ -0,0 +1,15 @@
+//
+//  CalenderCell.swift
+//  eventCal
+//
+//  Created by Harsh_Dev on 2019-07-01.
+//  Copyright © 2019 FitFi. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+class CalenderCell: JTAppleCell {
+    
+    @IBOutlet weak var selectedView: UIView!
+    @IBOutlet weak var dateLabel: UILabel!
+}
